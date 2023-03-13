523bcd1febf83e4a556826041dd19bcff17a84fd
ABSOLUTELYPERFECT <sc2liink@gmail.com>
2023-03-14 08:06:53 +0900

    Добавьте файлы проекта.

---
+++ iJuniorTestDrive/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iJuniorTestDrive
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int integerVariable = new int();
+            long longVariable = new long();
+            float floatVariable = new float();
+            string stringVariable = "";
+            char charVariable = ' ';
+            short shortVariable = new short();
+            char[] charArrayVariable  = new char[1];
+            bool boolVariable = true;
+            double doubleVariable = new double();
+            List<string> stringListVariable = new List<string>();
+
+        }
+    }
+}
